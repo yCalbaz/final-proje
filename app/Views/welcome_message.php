f3d4f92cdd6ad34755992e03b802beb2c7a908f9
unknown <zehrasaribastanoglu@gmail.com>
2024-11-24 15:38:11 +0300

    nefknk

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>KAYZfhngngvE</title>
+    <title>KAYZfhngngvmödvklkmkkfvlkE</title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
