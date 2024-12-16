c43c2dc585c8034331c087d749847517a5ea54cf
example <email@example.com>
2024-12-16 03:16:56 +0300

    Update anasayfa.php

--- app/Views/anasayfa.php
+++ app/Views/anasayfa.php
@@ -6,7 +6,7 @@
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
-    <link rel="stylesheet" href="css/anasayfa.css">
+    <link rel="stylesheet" href="sayfalar/css/anasayfa.php">
     <link rel="stylesheet" href="<?= base_url('css/responsive.css'); ?>"> 
 
     <!-- STYLES -->
