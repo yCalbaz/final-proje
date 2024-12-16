9d65f06db4f4737f3d613aa368bb74157bb22547
example <email@example.com>
2024-12-16 04:03:58 +0300

    kader

--- app/Views/tema/header.php
+++ app/Views/tema/header.php
@@ -1,9 +1,16 @@
-<!doctype html>
-<html>
-<head>
-    <title>CodeIgniter Tutorial</title>
-</head>
-<body>
-
-    <h1><?= $title ?></h1>
-    <h2><?= $isim ?></h2>
+<header>
+    <div class="container">
+        
+        <nav>
+            <ul>
+                <li><a href="index.php">Anasayfa</a></li>
+                <li><a href="urunler.php">Ürünler</a></li>
+                <li><a href="hakkimizda.php">Hakkımızda</a></li>
+                <li><a href="iletisim.php">İletişim</a></li>
+                <li><a href="sepet_view.php">Sepetim</a></li>
+                <li><a href="giris.php" class="btn-login">Giriş Yap</a></li>
+                <li><a href="uye-ol.php" class="btn-signup">Üye Ol</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
